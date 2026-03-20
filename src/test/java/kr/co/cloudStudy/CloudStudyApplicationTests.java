4e974aaca2028c5869d5021bbbffbd824648e94e
ixxveon <igayeon322s@gmail.com>
2026-03-20 16:50:07 +0900

    백엔드 초기 세팅

---
+++ src/test/java/kr/co/cloudStudy/CloudStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.cloudStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
