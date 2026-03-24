bddb90442f585dd140195eee3723af11a3503819
Bora <sbr7518@gmail.com>
2026-03-25 00:04:25 +0900

    집에서컨트롤러 생성

--- src/main/java/kr/co/cloudStudy/department/controller/DepartController.java
+++ src/main/java/kr/co/cloudStudy/department/controller/DepartController.java
@@ -1,5 +1,64 @@
 package kr.co.cloudStudy.department.controller;
 
-public class DepartController {
+import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import kr.co.cloudStudy.department.dto.ReqDeptDTO;
+import kr.co.cloudStudy.department.dto.ResDeptDTO;
+import kr.co.cloudStudy.department.service.DepartService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/departments")
+@RequiredArgsConstructor
+@Tag(name = "Department", description = "부서 관리 API")
+public class DepartController {
+	
+	private final DepartService departService;
+	
+	// 부서 등록 (POST)
+	@PostMapping
+	public ResponseEntity<Long> register(@RequestBody ReqDeptDTO dto) {
+		Long id = departService.register(dto);
+		return ResponseEntity.ok(id);
+	}
+	
+	// 부서 전체 목록 조회 (GET)
+	@GetMapping
+	public ResponseEntity<List<ResDeptDTO>> getList() {
+		List<ResDeptDTO> list = departService.getList();
+		return ResponseEntity.ok(list);
+	}
+	
+	// 부서 상세 조회(GET)
+	@GetMapping("/{id}")
+	public ResponseEntity<ResDeptDTO> read(@PathVariable Long id) {
+		ResDeptDTO dto = departService.read(id);
+		return ResponseEntity.ok(dto);
+	}
+	
+	// 부서 정보 수정 (PUT)
+	@PutMapping
+	public ResponseEntity<Void> modify(@RequestBody ReqDeptDTO dto) {
+		departService.modify(dto);
+		return ResponseEntity.ok().build();
+	}
+	
+	// 부서 삭제 (DELETE)
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> remove(@PathVariable Long id) {
+		departService.remove(id);
+		return ResponseEntity.ok().build();
+	}
+	
 }
--- src/main/java/kr/co/cloudStudy/department/entity/Department.java
+++ src/main/java/kr/co/cloudStudy/department/entity/Department.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -50,7 +51,7 @@ public class Department {
 	}
 	
 	// 데이터 수정 전 수정일 자동 실행
-	@PrePersist
+	@PreUpdate
 	protected void onUpdate() {
 		this.createdAt = LocalDateTime.now();
 	}
