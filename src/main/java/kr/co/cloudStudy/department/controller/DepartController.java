c2d0cd814d09a39ac9b690d6ef4ec8499b5fc9c7
Bora <sbr7518@gmail.com>
2026-03-25 16:09:53 +0900

    docs: 컨트롤러 모든 메서드 @Operation 스웨거 문서화 적용

--- src/main/java/kr/co/cloudStudy/department/controller/DepartController.java
+++ src/main/java/kr/co/cloudStudy/department/controller/DepartController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.co.cloudStudy.department.dto.ReqDeptDTO;
 import kr.co.cloudStudy.department.dto.ResDeptDTO;
@@ -27,13 +28,15 @@ public class DepartController {
 	private final DepartService departService;
 	
 	// 부서 등록 (POST)
+	@Operation(summary = "부서 등록" , description = "새로운 부서 정보를 시스템에 저장하고 생성된 PK(deptid)를 반환합니다.")
 	@PostMapping
 	public ResponseEntity<Long> register(@RequestBody ReqDeptDTO dto) {
-		Long id = departService.register(dto);
-		return ResponseEntity.ok(id);
+		Long deptid = departService.register(dto);
+		return ResponseEntity.ok(deptid);
 	}
 	
 	// 부서 전체 목록 조회 (GET)
+	@Operation(summary = "부서 목록 조회" , description = "등록된 모든 부서의 리스트를 조회합니다.")
 	@GetMapping
 	public ResponseEntity<List<ResDeptDTO>> getList() {
 		List<ResDeptDTO> list = departService.getList();
@@ -41,13 +44,15 @@ public class DepartController {
 	}
 	
 	// 부서 상세 조회(GET)
-	@GetMapping("/{id}")
-	public ResponseEntity<ResDeptDTO> read(@PathVariable Long id) {
-		ResDeptDTO dto = departService.read(id);
+	@Operation(summary = "부서 상세 조회", description = "부서 ID(deptid)를 통해 특정 부서 정보를 상세 조회합니다.")
+	@GetMapping("/{deptid}")
+	public ResponseEntity<ResDeptDTO> read(@PathVariable Long deptid) {
+		ResDeptDTO dto = departService.read(deptid);
 		return ResponseEntity.ok(dto);
 	}
 	
 	// 부서 정보 수정 (PUT)
+	@Operation(summary = "부서 정보 수정", description = "기존 부서의 정보(내용)를 업데이트합니다.")
 	@PutMapping
 	public ResponseEntity<Void> modify(@RequestBody ReqDeptDTO dto) {
 		departService.modify(dto);
@@ -55,9 +60,10 @@ public class DepartController {
 	}
 	
 	// 부서 삭제 (DELETE)
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> remove(@PathVariable Long id) {
-		departService.remove(id);
+	@Operation(summary = "부서 삭제", description = "부서 ID(deptid)를 이용해 해당 부서 데이터를 삭제합니다.")
+	@DeleteMapping("/{deptid}")
+	public ResponseEntity<Void> remove(@PathVariable Long deptid) {
+		departService.remove(deptid);
 		return ResponseEntity.ok().build();
 	}
 	
